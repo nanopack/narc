1700129d0ba8735050925c122e141f5748abe7e4
Daniel Barney <daniel@pagodabox.com>
2014-05-21 10:29:49 -0600

    removed a few odd functions

--- src/util.h
+++ src/util.h
@@ -38,7 +38,6 @@ long long memtoll(const char *p, int *err);
 int ll2string(char *s, size_t len, long long value);
 int string2ll(const char *s, size_t slen, long long *value);
 int string2l(const char *s, size_t slen, long *value);
-int d2string(char *buf, size_t len, double value);
 sds getAbsolutePath(char *filename);
 int pathIsBaseName(char *path);
 
